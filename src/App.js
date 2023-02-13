e8a0c56c7840e836e1d5e211d655b6c2d3298dc9
raycode21 <wanjiku.rachel21@gmail.com>
2023-02-13 09:32:31 +0300

    Add task and save functionality added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import Header from './components/Header';
+import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState(
@@ -43,6 +44,13 @@ function App() {
     }
   ])
 
+//Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+
+  }
   //Toggle reminder
   const toggleReminder = (id) => {
     setTasks(tasks.map((task) => task.id===id ? {
@@ -50,16 +58,17 @@ function App() {
     
   
   }
-
-
-  //Delete Task
+ //Delete Task
   const deleteTask = (id) => { 
     setTasks(tasks.filter((task) => task.id !== id))
+     
      }
+
 
   return (
     <div className="container">
       <Header/>
+      <AddTask onAdd={addTask}/>
       { tasks.length > 0 ? (<Tasks tasks = {tasks} 
         onDelete={deleteTask} onToggle={toggleReminder}/>
         ) : ( 'No tasks to show') }
